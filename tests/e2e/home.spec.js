ed0e3ad4f2081ceef465a9af179a3e01d7af7562
LoreSVi <loresuvivas@gmail.com>
2026-07-29 09:19:49 +0200

    test(e2e): query Ver detalle as link role in home.spec

--- tests/e2e/home.spec.js
+++ tests/e2e/home.spec.js
@@ -5,7 +5,7 @@ test("la pantalla principal carga y muestra botones Ver detalle", async ({
 }) => {
   await page.goto("/");
   await expect(
-    page.getByRole("button", { name: /ver detalle/i }).first(),
+    page.getByRole("link", { name: /ver detalle/i }).first(),
   ).toBeVisible();
 });
 
@@ -13,7 +13,7 @@ test("al hacer click en Ver detalle se navega a la vista de producto", async ({
   page,
 }) => {
   await page.goto("/");
-  const detalle = page.getByRole("button", { name: /ver detalle/i }).first();
+  const detalle = page.getByRole("link", { name: /ver detalle/i }).first();
   await expect(detalle).toBeVisible();
   await detalle.click();
   await expect(page).toHaveURL(/\/itemDetail\//);
